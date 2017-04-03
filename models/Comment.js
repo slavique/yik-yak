f4038bfe3223bca1aaef3552f6d76dc928f12c64
Viacheslav_Valitskas <valitskas@gmail.com>
2017-04-03 19:07:43 +0300

    second

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,9 @@
-ECHO is on.
+var mongoose = require('git status');
+
+var CommentSchema = new mongoose.Schema({
+	username : {type:String, default: ''},
+	body : {type:String, default: ''},
+	timestamp : {type:Date, default: Date.now}
+});
+
+module.exports = mongoose.model('CommentSchema', CommentSchema);
--- models/Zone.js
+++ models/Zone.js
@@ -1,1 +1,9 @@
-ECHO is on.
+var mongoose = require('mongoose');
+
+var ZoneSchema = new mongoose.Schema({
+	name : {type:String, default: ''},
+	zipCodes : {type:Array, default: []},
+	timestamp : {type:Date, default: Date.now}
+});
+
+module.exports = mongoose.model('ZoneSchema', ZoneSchema);
